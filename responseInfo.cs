8e2f43352e71a4e0603dd0f64a718dee38cd4b1f
julio.luna <julio.luna@GTPZ1SILAPA37>
2024-07-23 11:14:18 -0600

    Add project files.

---
+++ responseInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sqs_aws
+{
+    public class responseInfo
+    {
+        public  int Identificador { get; set; }
+        public string? Info { get; set; }
+    }
+}
